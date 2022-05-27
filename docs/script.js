86eb1a96f15c74a1c01b0ab480e3f89a423cdbb3
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-05-27 18:42:20 -0400

    fad

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,11 @@
 function navbar(){
     document.location.href = "https://www.youtube.com/watch?v=sSSOG8g7PFg";
 }
+
+function bottom_about(){
+    document.getElementsByClassName("bottom_about").innerHTML="feosif";
+    setTimeout(1000);
+    document.getElementsByClassName("bottom_about").innerHTML="About";
+    document.getElementById("ahhh").innerHTML="feosif";
+    console.log("AHHHHHHHHHHHHHHH");
+}
