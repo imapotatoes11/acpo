ec52c41d3f4a37c212ec92b58ca7948cb22eeba9
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-06-16 18:48:41 -0400

    Create alexNoDelete.js

---
+++ alexNoDelete.js
@@ -0,0 +1,6 @@
+//when they click subscribe to our newsletter it clears the form
+//thats literally all this does
+//pretty sure it doesn't even work tbh
+function clearaaaaa(){
+  document.getElementsByClassName('newsletter-form-field-element field-element field-control').value=''
+}
