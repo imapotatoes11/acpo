449f55919121f07be60c9b0e2418a456db0ad884
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-06-16 18:52:44 -0400

    stuff

--- alexNoDelete.js
+++ alexNoDelete.js
@@ -2,5 +2,7 @@
 //thats literally all this does
 //pretty sure it doesn't even work tbh
 function clearaaaaa(){
-  document.getElementsByClassName('newsletter-form-field-element field-element field-control').value=''
+  document.getElementById('email1').value='';
+  document.getElementById('email2').value='';
+  document.getElementById('email-yui_3_17_2_1_1552579698432_3881-field').value='';
 }
