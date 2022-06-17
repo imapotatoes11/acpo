8f6f3f408339527e1e84f6cd762775948311449f
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-06-17 09:08:21 -0400

    uhhihiu

--- alexNoDelete.js
+++ alexNoDelete.js
@@ -1,7 +1,7 @@
 //when they click subscribe to our newsletter it clears the form
 //thats literally all this does
 //pretty sure it doesn't even work tbh
-function clearaaaaa(){
+function clearaaaaa(a,b,c){
   document.getElementById('email1').value='';
   document.getElementById('email2').value='';
   document.getElementById('email-yui_3_17_2_1_1552579698432_3881-field').value='';
