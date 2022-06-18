4fb81a17d4fc86190988e89534cdf689feae6beb
imaegg11 <95944377+imaegg11@users.noreply.github.com>
2022-06-18 13:44:03 -0400

    Add files via upload

--- alexNoDelete.js
+++ alexNoDelete.js
@@ -2,9 +2,9 @@
 //thats literally all this does
 //pretty sure it doesn't even work tbh
 function clearaaaaa(){
-  console.log("clearaaaaa()")
-  document.getElementById('email1').value='';
-  document.getElementById('email2').value='';
-  document.getElementById('email-yui_3_17_2_1_1552579698432_3881-field').value='';
-  console.log("ahhhhhuidfgasodfoasdjfiasdjfojasdofijsoidfj");
+  a = document.getElementById('email1').value;
+  b = document.getElementById('email2').value;
+  c = document.getElementById('email-yui_3_17_2_1_1552579698432_3881-field').value;
+  console.log(a, b, c);
+  console.log("test")
 }
